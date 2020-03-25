e5d5b30702a521cb723dedbafbef759205b73453
Jarod T490 <jarod.zheng@outlook.com>
2020-03-25 15:11:13 +0800

    Add TempConvert

---
+++ venv/TempConvert.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*- 
+"""
+Project: PyLib
+Author: Jarod
+Create time: 2020-03-25 14:54
+IDE: PyCharm
+Introduction:
+"""
+
+TempStr = input ("请输入带有符号的温度值：")
+if TempStr[-1] in ['F', 'f']:
+    C = (eval(TempStr[0:-1])-32)/1.8
+    print("转换后的温度{:.2f}C".format(C))
+elif TempStr[-1] in ['C', 'c']:
+    F = 1.8*eval(TempStr[0:-1])+32
+    print("转换后的温度{:.2f}F".format(F))
+else:
+    print("输入格式错误")
