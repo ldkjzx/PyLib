77d12d3ba6146f955dc179f9e4a04b81ec0095a8
Jarod T490 <jarod.zheng@outlook.com>
2020-04-01 18:51:54 +0800

    initial TurtleRabbit

---
+++ 02-1 TurtleRabbit.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*- 
+"""
+Project: PyLib
+Author: Jarod
+Create time: 2020-04-01 18:39
+IDE: PyCharm
+Introduction:
+"""
+
+import turtle
+
+turtle.setup(800,800,200,200)
+turtle.penup()
+turtle.goto(189,50)
+turtle.pendown()
+turtle.pencolor("purple") # 海龟颜色
+
+turtle.goto(189,50)
+turtle.goto(182,74)
+turtle.goto(174,48)
+turtle.goto(189,50)
+turtle.goto(174,48)
+turtle.goto(157,69)
+turtle.goto(182,74)
+turtle.goto(174,86)
+turtle.goto(157,69)
+turtle.goto(142,102)
+turtle.goto(174,86)
+turtle.goto(189,83)
+turtle.goto(190,91)
+turtle.goto(174,86)
+turtle.goto(190,91)
+turtle.goto(190,101)
+turtle.goto(174,86)
+turtle.goto(169,101)
+turtle.goto(190,101)
+turtle.goto(174,118)
+turtle.goto(169,101)
+turtle.goto(142,102)
+turtle.goto(174,118)
+turtle.goto(156,133)
+turtle.goto(142,102)
+turtle.goto(134,108)
+turtle.goto(156,133)
+
+tuttle.done()
+
