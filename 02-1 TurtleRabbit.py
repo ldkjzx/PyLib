05b6f8a40a12953788f940cbc2b0f663d9196d43
Jarod T490 <jarod.zheng@outlook.com>
2020-04-01 21:46:54 +0800

    update TurtleRabbit

--- 02-1 TurtleRabbit.py
+++ 02-1 TurtleRabbit.py
@@ -9,6 +9,7 @@ Introduction:
 
 import turtle
 
+'''
 turtle.setup(800,800,200,200)
 turtle.penup()
 turtle.goto(189,50)
@@ -43,5 +44,37 @@ turtle.goto(142,102)
 turtle.goto(134,108)
 turtle.goto(156,133)
 
-tuttle.done()
+turtle.done()
+'''
 
+def Beat(n,a):
+    turtle.seth(a) # 设置海龟绝对角度
+    turtle.fd(n)
+    turtle.seth(-a)
+    turtle.fd(2*n)
+    turtle.seth(a)
+    turtle.fd(3*n)
+    turtle.seth(-a)
+    turtle.fd(4*n)
+    turtle.seth(a)
+    turtle.fd(3*n)
+    turtle.seth(-a)
+    turtle.fd(2*n)
+    turtle.seth(a)
+    turtle.fd(n)
+    turtle.seth(0)
+
+if __name__ == '__main__':
+    turtle.setup(650, 350, 200, 200) # 设置窗口大小，和屏幕绝对位置
+    turtle.penup() # 画笔抬起，海龟飞行
+    turtle.fd(-280) # 海龟从原点移动
+    turtle.pendown() # 画笔放下，海龟爬行
+    turtle.pensize(2) # 海龟腰围
+    turtle.pencolor("red") # 海龟颜色
+    turtle.fd(65)
+
+    for i in range(4):
+        Beat(40, 85)
+        turtle.fd(65)
+
+    turtle.done()
