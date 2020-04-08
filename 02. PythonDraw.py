96c872d62c496672e9f60cf2ac3621ed6b71eb54
Jarod T490 <jarod.zheng@outlook.com>
2020-04-08 17:00:19 +0800

    initial daydayup

--- 02. PythonDraw.py
+++ 02. PythonDraw.py
@@ -7,7 +7,9 @@ IDE: PyCharm
 Introduction:
 """
 
-import turtle
+# import turtle         # 无库名冲突，但代码冗余
+# from turtle import *   # 易产生库名冲突
+import turtle as t # 库别名
 
 # 海龟角度坐标
 '''
@@ -28,24 +30,18 @@ turtle.goto(-100, 100)
 turtle.goto(0,0)
 '''
 
-
-turtle.setup(650, 350, 200, 200) # 设置窗口大小，和屏幕绝对位置
-turtle.penup() # 画笔抬起，海龟飞行
-turtle.fd(-250) # 海龟从原点移动
-turtle.pendown() # 画笔放下，海龟爬行
-turtle.pensize(25) # 海龟腰围
-turtle.pencolor("purple") # 海龟颜色
-turtle.seth(-40) # 设置海龟绝对角度
+t.setup(650, 350, 200, 200)  # 设置窗口大小，和屏幕绝对位置
+t.pu()  # 画笔抬起，海龟飞行
+t.fd(-250)  # 海龟从原点移动
+t.pd()  # 画笔放下，海龟爬行
+t.pensize(25)  # 海龟腰围
+t.pencolor("purple")  # 海龟颜色
+t.seth(-40)  # 设置海龟绝对角度
 for i in range(4):
-    turtle.circle(40, 80) # 海龟曲线前进，r + 弧度
-    turtle.circle(-40, 80)
-turtle.circle(40, 80/2)
-turtle.fd(40) # 海龟直线前进
-turtle.circle(16, 180)
-turtle.fd(40 * 2/3)
-turtle.done()
-
-
-
-
-
+    t.circle(40, 80)  # 海龟曲线前进，r + 弧度
+    t.circle(-40, 80)
+t.circle(40, 80 / 2)
+t.fd(40)  # 海龟直线前进
+t.circle(16, 180)
+t.fd(40 * 2 / 3)
+t.done()
---
+++ 03. DayDayUp.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*- 
+"""
+Project: PyLib
+Author: Jarod
+Create time: 2020-04-08 16:54
+IDE: PyCharm
+Introduction:
+"""
+
+dayup = pow(1.001, 365)
+daydown = pow(0.999, 365)
+
+print("天天向上的力量{:.3}".format(dayup))
+print("天天向下的力量{:.3}".format(daydown))
