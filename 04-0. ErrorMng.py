7e8a8e61606cf2e5dd0570330fa4d69cbff4e049
Jarod T490 <jarod.zheng@outlook.com>
2020-04-10 13:47:53 +0800

    initial calBMI

--- 04-0. ErrorMng.py
+++ 04-0. ErrorMng.py
@@ -7,8 +7,28 @@ IDE: PyCharm
 Introduction:
 """
 
+'''
 try:
-    num = eval(input("请输入："))
-    print(num**2)
+    guess = eval(input("请输入："))
+    print("猜{}了！".format("对对" if guess==99 else "错错"))
+
+
+    #if guess>99 or guess<99:
+    #    print("猜错了！")
+    #else:
+    #    print("猜对了！")
+
 except:
     print("输入的不是整数！")
+'''
+
+score = eval(input())
+if score >= 90:
+    grade = "A"
+elif score >= 80:
+    grade = "B"
+elif score >= 70:
+    grade = "C"
+elif score >= 60:
+    grade = "D"
+print("输入成绩属于级别{}".format(grade))
